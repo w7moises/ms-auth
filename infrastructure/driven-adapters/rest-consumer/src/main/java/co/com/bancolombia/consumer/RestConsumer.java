68c31a6d700b55c80b0709a147780a2d34021312
Walter Moises Moises Molina <w7moises@gmail.com>
2025-08-24 22:24:11 -0500

    sonar recomendations

--- infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/bancolombia/consumer/RestConsumer.java
+++ infrastructure/driven-adapters/rest-consumer/src/main/java/co/com/bancolombia/consumer/RestConsumer.java
@@ -8,12 +8,9 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-public class RestConsumer /* implements Gateway from domain */{
+public class RestConsumer /* implements Gateway from domain */ {
     private final WebClient client;
 
-
-    // these methods are an example that illustrates the implementation of WebClient.
-    // You should use the methods that you implement from the Gateway from the domain.
     @CircuitBreaker(name = "testGet" /*, fallbackMethod = "testGetOk"*/)
     public Mono<ObjectResponse> testGet() {
         return client
@@ -22,20 +19,12 @@ public class RestConsumer /* implements Gateway from domain */{
                 .bodyToMono(ObjectResponse.class);
     }
 
-// Possible fallback method
-//    public Mono<String> testGetOk(Exception ignored) {
-//        return client
-//                .get() // TODO: change for another endpoint or destination
-//                .retrieve()
-//                .bodyToMono(String.class);
-//    }
-
     @CircuitBreaker(name = "testPost")
     public Mono<ObjectResponse> testPost() {
         ObjectRequest request = ObjectRequest.builder()
-            .val1("exampleval1")
-            .val2("exampleval2")
-            .build();
+                .val1("exampleval1")
+                .val2("exampleval2")
+                .build();
         return client
                 .post()
                 .body(Mono.just(request), ObjectRequest.class)
